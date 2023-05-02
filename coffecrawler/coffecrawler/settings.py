4061b89af7a68f25fd719b6634831b3f2380cd82
AliKYH <114059225+AliKYH@users.noreply.github.com>
2023-05-02 16:05:22 +0200

    Budget crawler. Build the .json with test_spider

--- coffecrawler/coffecrawler/settings.py
+++ coffecrawler/coffecrawler/settings.py
@@ -63,7 +63,7 @@ ROBOTSTXT_OBEY = True
 # Configure item pipelines
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
 #ITEM_PIPELINES = {
-#    'coffecrawler.pipelines.DuplicatesPipeline': 100,
+#    'coffecrawler.pipelines.DuplicateItemPipeline': 100,
 #}
 
 # Enable and configure the AutoThrottle extension (disabled by default)
---
+++ test_spider.py
@@ -0,0 +1,26 @@
+from scrapy.crawler import CrawlerProcess
+from coffecrawler.coffecrawler.spiders.coffeespider import CoffeespiderSpider
+import json
+
+class Utf8JsonPipeline:
+    def open_spider(self, spider):
+        self.file = open('data.json', 'w', encoding='utf-8')
+
+    def close_spider(self, spider):
+        self.file.close()
+
+    def process_item(self, item, spider):
+        line = json.dumps(dict(item), ensure_ascii=False) + "\n"
+        self.file.write(line)
+        return item
+
+
+process = CrawlerProcess(settings={
+    'ITEM_PIPELINES': {
+        '__main__.Utf8JsonPipeline': 300,
+    },
+    'FEED_EXPORT_ENCODING': 'utf-8',
+})
+
+process.crawl(CoffeespiderSpider)
+process.start()
